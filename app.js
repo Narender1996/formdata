2b6ab8064fb77f1a62a04f17b4bd95e66fb2ad88
narender bisht <narender007bisht@gmail.com>
2020-02-07 02:03:21 -0800

    second change

--- app.js
+++ app.js
@@ -3,19 +3,42 @@ const app=express();
 const path=require('path')
 const bodyparser=require('body-parser')
 
+const sendmail=require('./mail')
+/*
+const mongoclient=require(mongodb).mongoclient;
+const assert=require('assert')
+const url='mongodb://localhost:27017'
 
-app.use(express.static('index'))
-app.use(bodyparser.urlencoded({extended:true}))
+const dbName='myproject';
+mongoclient.connect(url,function(err,client)
+{
+    assert.equal(null,err);
+    console.log("connect succefully");
+    const db=client.db(dbname)
+})
+*/
+app.use(express.static('__dirname'))
+app.use(bodyparser.urlencoded({extended:true}));
 
-app.get('/form',(req,res)=>{
+app.get('/',(req,res)=>{
 
     res.sendFile( path.join(__dirname,'index.html'))
-})
+});
 
-app.post('/form',(req,res)=>{
-    console.log(res.body);
-    res.json('submit')
-})
+app.post('/',(req,res)=>{
+   // console.log('data', req.body);
+    const  {emailid,fname,lname}=req.body
+    console.log(req.body);
+    sendMail(emailid,fname,lname,(err,data)=>{
+        if(err)
+        {
+            res.status(500).json({message:'internal error'});
+        }
+            else{
+                res.json({message:'mail send',data})
+            }
+    });
+    });
 
 /*
 app.get('/api/courses/:id',(req,res)=>
@@ -29,6 +52,6 @@ app.get('/api/:id',(req,res)=>{
 });
 */
 
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 3001;
 
 app.listen(port,()=>{console.log(`server start at port ${port}`)})
